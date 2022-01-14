e06369da8dbb4507f5b58fdce720b67692fe0625
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-13 20:37:34 -0500

    Create script to get all tasks from project

---
+++ getNearestTaskStatus.py
@@ -0,0 +1,23 @@
+from config import PERSONAL_ACCESS_TOKEN, PROJECT_GID, WORKSPACE_GID, AP, RK
+import asana
+
+
+def get_tasks(client):
+    params = {
+        'project': PROJECT_GID,
+        'limit': 100,
+        'opt_fields': ['assignee', 'due_on']
+    }
+
+    return client.tasks.get_tasks(params)
+
+
+def main():
+    client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
+    tasks = get_tasks(client, AP.gid)
+    for task in tasks:
+        print(task)
+
+
+if __name__ == '__main__':
+    main()
