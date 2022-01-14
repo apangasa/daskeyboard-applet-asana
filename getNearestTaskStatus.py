33bceefbe858711503789dde4a8e4f0037d4860c
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-13 20:38:08 -0500

    Create script to get tasks by assignee and workspace

--- getNearestTaskStatus.py
+++ getNearestTaskStatus.py
@@ -12,6 +12,17 @@ def get_tasks(client):
     return client.tasks.get_tasks(params)
 
 
+def get_tasks_by_user(client, user_gid):
+    params = {
+        'workspace': WORKSPACE_GID,
+        'assignee': user_gid,
+        'limit': 100,
+        'opt_fields': ['assignee', 'due_on', 'projects']
+    }
+
+    return client.tasks.get_tasks(params)
+
+
 def main():
     client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
     tasks = get_tasks(client, AP.gid)
