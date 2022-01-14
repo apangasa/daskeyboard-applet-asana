69ad8b0c9802f4c1d71711f989c604f477a46f69
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-14 14:19:57 -0500

    Modify signal details

--- applet.py
+++ applet.py
@@ -1,7 +1,8 @@
-from config import DASQ_BACKEND_URL, DASQ_HEADERS, DASQ_SIGNALS_ROUTE
+from config import DASQ_BACKEND_URL, DASQ_HEADERS, DASQ_SIGNALS_ROUTE, WORKSPACE_NAME
 import getNearestTaskStatus
 import json
 import requests
+import time
 
 
 def main():
@@ -10,10 +11,11 @@ def main():
         'zoneId': 'KEY_K',
         'color': color_hex,
         'effect': 'SET_COLOR',
-        'pid': 'DK5QPID',
+        'pid': 'DK4QPID',
         'clientName': 'Python script',
-        'message': 'Placeholder message',
-        'name': 'Placeholder name'
+        'message': '',
+        'name': f'New task in {WORKSPACE_NAME}',
+        'isMuted': True
     }
 
     signal = json.dumps(signal)
