c6be70df4a7f2919dd3857aae7f1472f87946a0b
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-13 20:41:09 -0500

    Create method to filter tasks by project
    Also added task name to list of return fields.

--- getNearestTaskStatus.py
+++ getNearestTaskStatus.py
@@ -6,7 +6,7 @@ def get_tasks(client):
     params = {
         'project': PROJECT_GID,
         'limit': 100,
-        'opt_fields': ['assignee', 'due_on']
+        'opt_fields': ['name', 'assignee', 'due_on']
     }
 
     return client.tasks.get_tasks(params)
@@ -17,15 +17,23 @@ def get_tasks_by_user(client, user_gid):
         'workspace': WORKSPACE_GID,
         'assignee': user_gid,
         'limit': 100,
-        'opt_fields': ['assignee', 'due_on', 'projects']
+        'opt_fields': ['name', 'assignee', 'due_on', 'projects']
     }
 
     return client.tasks.get_tasks(params)
 
 
+def filter_tasks_by_project(tasks, project_gid=PROJECT_GID):
+    def part_of_project(task, project_gid):
+        return project_gid in map(lambda project: project['gid'], task['projects'])
+
+    return filter(lambda task: part_of_project(task, project_gid), tasks)
+
+
 def main():
     client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
-    tasks = get_tasks(client, AP.gid)
+    tasks = get_tasks_by_user(client, AP.gid)
+    tasks = filter_tasks_by_project(tasks)
     for task in tasks:
         print(task)
 
