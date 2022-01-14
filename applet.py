b620cff1ce1cd6e3207e79190ccbf6b5d9f96876
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-13 21:38:40 -0500

    Create script to send signal to keyboard

---
+++ applet.py
@@ -0,0 +1,31 @@
+from config import BACKEND_URL, DASQ_HEADERS
+import getNearestTaskStatus
+import json
+import requests
+
+
+def main():
+    color_hex = getNearestTaskStatus.main().web
+    signal = {
+        'zoneId': 'KEY_K',
+        'color': color_hex,
+        'effect': 'SET_COLOR',
+        'pid': 'DK5QPID',
+        'clientName': 'Python script',
+        'message': 'Placeholder message',
+        'name': 'Placeholder name'
+    }
+
+    signal = json.dumps(signal)
+
+    response = requests.post(
+        BACKEND_URL + '/api/1.0/signals', data=signal, headers=DASQ_HEADERS)
+
+    if response.ok:
+        print('Success!')
+
+    print(response.text)
+
+
+if __name__ == '__main__':
+    main()
